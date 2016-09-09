ae2a3704ec5eebd06f46ea7deb85b9ba3603a52a
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-09 16:08:44 +0300

    Setup the zizaco-entrust package

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
 
     ],
 
@@ -224,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
