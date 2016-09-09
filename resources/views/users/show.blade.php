3e03889cbd38bd6ac466ed235ed17a9aee94111b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-09 17:11:42 +0300

    Retrieve the users

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                @include('common.side_bar')
+            </div>
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="{{ url('/users/create') }}" class="btn btn-primary pull-right"><i
+                                    class="fa fa-plus"></i>
+                            Add</a>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Show a User</div>
+                            <div class="panel-body">
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
