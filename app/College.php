b75bbd8ce0d405b44c3fe204ffb6b09e1c96f85f
Mikaeel Shemu <crshemu@gmail.com>
2016-09-19 12:06:48 +0300

    Create School seeder and add relations between college and school

--- app/College.php
+++ app/College.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class College extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    public function schools()
+    {
+        return $this->hasMany(School::class);
+    }
 }
--- app/School.php
+++ app/School.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     protected $fillable = ['name', 'description'];
+
+    public function college()
+    {
+        return $this->belongsTo(College::class);
+    }
 }
