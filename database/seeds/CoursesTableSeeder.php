916c851ddef95bf76fc1cc0b40fdc08ff2ca8c4c
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-19 16:15:48 +0300

    Create CoursesTableSeeder

---
+++ database/seeds/CoursesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('courses')->insert([
+            'name' => 'Introduction to High Level Programming',
+            'code' => 'CS 110',
+            'credit' => 10,
+        ]);
+    }
+}
